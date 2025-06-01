3e6efeff08df9873c44fb77d41adc443b4db7ff2
Juan D. Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-01 07:26:52 +0200

    feat: add tokenize_text to transform:utils.py

--- transform/transform_utils.py
+++ transform/transform_utils.py
@@ -9,6 +9,7 @@ named Alice.txt and saved in the raw_data folder.
 
 from pathlib import Path
 import re
+from typing import List
 
 def read_text_file(file_path: Path, preview: bool = False) -> str:
     """
@@ -24,6 +25,7 @@ def read_text_file(file_path: Path, preview: bool = False) -> str:
     if not file_path.exists():
         raise FileNotFoundError(f"The file {file_path} does not exist.")
     text = file_path.read_text(encoding='utf-8')
+    print(" ✅ Text succesfully recognized")
     if preview:
         print("Preview of raw file content:")
         print(text[:500])  # Show the first 500 characters
@@ -45,20 +47,45 @@ def clean_text_file(text: str, preview: bool = False ) :
     """
     text = text.lower()
     text = re.sub(r"[.,!?;:¿¡()\"“”‘’'-]", "", text)  # Remove punctuation
-    text = re.sub(r"\s+", " ", text).strip()  # Remove extra spaces
+    cleaned_text = re.sub(r"\s+", " ", text).strip()  # Remove extra spaces
+    print(" ✅ Text succesfully cleaned")
 
     if preview:
         print("Preview of raw file content:")
-        print(text[:500])  # Show the first 500 characters
+        print(cleaned_text[:500])  # Show the first 500 characters
 
-    return text
+    return cleaned_text
+
+def tokenize_text (cleaned_text: str, preview: bool = False) -> List[str]:
+    """
+    Splits a cleaned text string into a list of word tokens.
+
+    Args:
+        text(str): The cleaned input text (no punctuation).
+        preview (bool): If True, prints the first 20 tokens.
+
+    Returns:
+        List[str]: The list of lowercase word tokens.
+    """
+    tokens = cleaned_text.split()
+    print(" ✅ Text succesfully tokenized")
+
+    if preview:
+        print("Preview of tokens:")
+        print(tokens[:20])
+
+    return tokens
+
+
+
 
 
 if __name__ == "__main__":
     # Example usage
     file_path = Path(__file__).parent.parent / "data/raw_data/Alice.txt"  # Replace with your actual file path
     try:
-        text = read_text_file(file_path, True)
-        text = clean_text_file(text, True)
+        text = read_text_file(file_path)
+        cleaned_text = clean_text_file(text)
+        tokens = tokenize_text(cleaned_text, True)
     except Exception as e:
         print(f"Error: {e}")
