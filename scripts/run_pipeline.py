7565cc9f93d804ffbfe0178b8072d59461486461
Juan Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-06-04 09:56:53 +0000

    docs: add docstring to run_pipeline.py, mileston: first file uploaded to s3

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,4 +1,12 @@
+"""
+This script uploads a local raw text file (Crime and Punishment) to an S3 bucket
+under the specified key. It uses the reusable `upload_to_s3` function from the
+s3_utils.s3_handler module.
+
+"""
+
 import sys
+
 from pathlib import Path
 
 # Add the root directory to the Python path
